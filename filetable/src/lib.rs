92f4fbaa98b34c885cffec2cbfb15b5b00dcfc15
Shi Lei <shi_lei@massclouds.com>
2024-11-11 04:01:22 +0000

    fix

--- filetable/src/lib.rs
+++ filetable/src/lib.rs
@@ -6,34 +6,75 @@ use alloc::vec::Vec;
 use axfile::fops::File;
 use mutex::Mutex;
 use spinpreempt::SpinLock;
+use axtype::{set_bit, clr_bit, O_CLOEXEC};
 
 pub struct FileTable {
     table: SlotVec<FileTableEntry>,
+    close_on_exec: usize,
 }
 
 impl FileTable {
     pub const fn new() -> Self {
         Self {
             table: SlotVec::new(),
+            close_on_exec: 0,
         }
     }
 
+    pub fn close_on_exec(&self) -> usize {
+        self.close_on_exec
+    }
+
     pub fn get_file(&self, fd: usize) -> Option<Arc<Mutex<File>>> {
+        if (fd as i32) < 0 {
+            return None;
+        }
         self.table
-            .get(fd-3)
+            .get(fd)
             .map(|entry| entry.file.clone())
     }
 
-    pub fn insert(&mut self, item: Arc<Mutex<File>>) -> usize {
+    pub fn insert(&mut self, item: Arc<Mutex<File>>, flags: usize) -> usize {
         let entry = FileTableEntry::new(item);
-        self.table.put(entry) + 3
+        let fd = self.table.put(entry);
+        if (flags & O_CLOEXEC as usize) != 0 {
+            set_bit(fd, &mut self.close_on_exec);
+        } else {
+            clr_bit(fd, &mut self.close_on_exec);
+        }
+        fd
     }
 
-    pub fn remove(&mut self, fd: usize) {
-        self.table.remove(fd-3);
+    pub fn remove(&mut self, fd: usize) -> Option<Arc<Mutex<File>>> {
+        clr_bit(fd, &mut self.close_on_exec);
+        self.table.remove(fd).map(|item| item.file)
+    }
+
+    pub fn alloc_fd(&mut self, start: usize) -> usize {
+        self.table.alloc_pos(start).unwrap()
+    }
+
+    pub fn fd_install(&mut self, pos: usize, file: Arc<Mutex<File>>) {
+        let entry = FileTableEntry::new(file.clone());
+        self.table.install(pos, entry)
+    }
+
+    pub fn slots_len(&self) -> usize {
+        self.table.slots.len()
+    }
+
+    pub fn copy_from(&mut self, src: &Self) {
+        self.close_on_exec = src.close_on_exec;
+        self.table.copy_from(&src.table)
+    }
+
+    pub fn reserve(&mut self, size: usize, num_occupied: usize) {
+        self.table.num_occupied = num_occupied;
+        self.table.reserve(size)
     }
 }
 
+#[derive(Clone)]
 pub struct FileTableEntry {
     file: Arc<Mutex<File>>,
 }
@@ -46,7 +87,7 @@ impl FileTableEntry {
     }
 }
 
-pub struct SlotVec<T> {
+pub struct SlotVec<T: Clone> {
     // The slots to store items.
     slots: Vec<Option<T>>,
     // The number of occupied slots.
@@ -54,7 +95,7 @@ pub struct SlotVec<T> {
     num_occupied: usize,
 }
 
-impl<T> SlotVec<T> {
+impl<T: Clone> SlotVec<T> {
     /// New an empty vector.
     pub const fn new() -> Self {
         Self {
@@ -62,6 +103,22 @@ impl<T> SlotVec<T> {
             num_occupied: 0,
         }
     }
+
+    pub fn copy_from(&mut self, src: &Self) {
+        self.slots = src.slots.clone();
+        self.num_occupied = src.num_occupied;
+    }
+
+    pub fn reserve(&mut self, size: usize) {
+        self.slots.resize(size, None)
+    }
+
+    /// Return the number of slots.
+    pub fn slots_len(&self) -> usize {
+        self.slots.len()
+    }
+
+    /// Get slot at index.
     pub fn get(&self, idx: usize) -> Option<&T> {
         if idx >= self.slots.len() {
             return None;
@@ -85,6 +142,10 @@ impl<T> SlotVec<T> {
         idx
     }
 
+    pub fn install(&mut self, pos: usize, entry: T) {
+        self.slots[pos] = Some(entry);
+    }
+
     /// Remove and return the item at position `idx`.
     ///
     /// Return `None` if `idx` is out of bounds or the item has been removed.
@@ -99,6 +160,21 @@ impl<T> SlotVec<T> {
             self.num_occupied -= 1;
         }
         del_item
+    }
+
+    /// Alloc a slot from 'start' postion
+    ///
+    /// Return `None` if no slot can be used or the postion.
+    pub fn alloc_pos(&mut self, mut start: usize) -> Option<usize> {
+        if start < self.slots.len() {
+            start = self.slots.len();
+        }
+        let mut pos = self.slots.len();
+        while pos <= start {
+            self.slots.push(None);
+            pos += 1;
+        }
+        Some(start)
     }
 }
 
